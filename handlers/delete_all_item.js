bb0c84d5f2aeefdff2b4368212a9c74e2d23ce19
pistachiyoda <fujisawamai21@gmail.com>
2018-07-22 00:34:04 +0900

    修正

--- handlers/delete_all_item.js
+++ handlers/delete_all_item.js
@@ -4,12 +4,7 @@ const DeleteItemAllIntentHandler = {
         && handlerInput.requestEnvelope.request.intent.name === 'DeleteItemAllIntent';
     },
     async handle(handlerInput) {
-        const attributes = await handlerInput.attributesManager.getPersistentAttributes();
-        //itemListにはこんな感じでアイテムオブジェクトの配列が入っている。[ { name: '牛乳' }, { name: 'コーヒー' } ] 
-        //0を代入してリセット
-        var itemList = attributes.itemList;
-        itemList = 0;
-        handlerInput.attributesManager.setPersistentAttributes({'itemList': itemList});
+        handlerInput.attributesManager.setPersistentAttributes({'itemList': [] });
         await handlerInput.attributesManager.savePersistentAttributes();
 
         const speechText = 'メモがリセットされました';
